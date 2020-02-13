2ef30a0de572f475388d694debe51c147abba4e9
Lukas Rauchenzauner <rauchi13.lr@gmail.com>
2020-02-13 10:31:44 +0100

    Fixed minor bugs

--- CashRegister/CashRegister.Shared/Model.cs
+++ CashRegister/CashRegister.Shared/Model.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text.Json.Serialization;
 
 namespace CashRegister.Shared
@@ -16,19 +18,52 @@ namespace CashRegister.Shared
         public decimal UnitPrice { get; set; }
     }
 
-    public class ReceiptLine
+    public class ReceiptLine: INotifyPropertyChanged
     {
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int IDValue;
         [JsonPropertyName("id")]
-        public int ID { get; set; }
+        public int ID
+        {
+            get => IDValue;
+            set
+            {
+                IDValue = value;
+                OnPropertyChanged(nameof(ID));
+            }
+        }
 
         [JsonPropertyName("product")]
         public Product Product { get; set; }
 
+        private int AmountValue;
         [JsonPropertyName("amount")]
-        public int Amount { get; set; }
-
+        public int Amount
+        {
+            get => AmountValue;
+            set
+            {
+                AmountValue = value;
+                OnPropertyChanged(nameof(Amount));
+            }
+        }
+        private decimal TotalPriceValue;
         [JsonPropertyName("totalPrice")]
-        public decimal TotalPrice { get; set; }
+        public decimal TotalPrice
+        {
+            get => TotalPriceValue;
+            set
+            {
+                TotalPriceValue = value;
+                OnPropertyChanged(nameof(TotalPrice));
+            }
+        }
     }
 
     public class Receipt
